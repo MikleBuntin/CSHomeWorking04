5b5ae24e0f725905383bf3328f81167bff886c78
MikleBuntin <BuntinMA@mail.ru>
2022-08-21 13:11:39 +0300

    Решил задачу 25

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+double aPowB = Math.Pow(a, b);
+
+// В задании написано: "и возводит в степень". А выводить не нужно? :)
+
+Console.WriteLine(aPowB);
